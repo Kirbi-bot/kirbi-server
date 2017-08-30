3edcafc531186f373d3cc4475c41841d606bb0c2
Nate Richardson <hello@naterichardson.com>
2017-08-30 16:32:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# kirbi-server module
+A module for [Kirbi](https://github.com/richardson-media-house/kirbi), that adds the !servers command, along with the ability to list out specific server info.
+
+## Usage
+
+Add a servers.json file to your `/config` folder in the core bot, an example is included with the core bot.
+
+- !servers => lists out all available server commands
+- !server [server_command_name] => prints out provided info on the server
---
+++ server.js
@@ -0,0 +1,33 @@
+module.exports = function (Kirbi) {
+	const servers = Kirbi.getJsonObject('/config/servers.json');
+	return {
+		commands: [
+			'server'
+		],
+		server: {
+			usage: `list|${servers.map(server => server.key).join('|')}`,
+			process: (msg, suffix, isEdit, cb) => {
+				if (suffix.toLowerCase() === 'list' || suffix.trim() === '') {
+					cb({
+						embed: {
+							title: `${Kirbi.Config.discord.serverName} Servers`,
+							description: servers.map(server => server.key).join('\n'),
+							color: Kirbi.Config.discord.defaultEmbedColor
+						}
+					}, msg);
+				} else {
+					const info = servers.filter(server => server.key === suffix.toLowerCase())[0];
+					if (info) {
+						cb({
+							embed: {
+								title: info.title,
+								description: info.description,
+								color: Kirbi.Config.discord.defaultEmbedColor
+							}
+						}, msg);
+					}
+				}
+			}
+		}
+	};
+};
